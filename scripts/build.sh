25f2069061172388f1addf360ebc206017421202
Tony Phipps <tonyphipps@gmail.com>
2016-12-30 22:59:14 +0000

    feat: add travis CI deployment

---
+++ scripts/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -x
+
+# Build the site with Gulp
+gulp build
---
+++ scripts/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -x
+if [ $TRAVIS_BRANCH == 'master' ] ; then
+    # Initialize a new git repo in _site, and push it to our server.
+    cd dist
+    git init
+
+    git remote add deploy "deploy@codepen.neutraltone.com:/var/www/codepen/html"
+    git config user.name "Travis CI"
+    git config user.email "tonyphipps+travisCI@gmail.com"
+
+    git add .
+    git commit -m "Deploy"
+    git push --force deploy master
+else
+    echo "Not deploying, since this branch isn't master."
+fi
