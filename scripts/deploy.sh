64f9b11035067d30be6f035d902ffaa8364692d4
Tony Phipps <tonyphipps@gmail.com>
2016-12-31 01:03:11 +0000

    fix: issue with deployment path

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
-  # Initialize a new git repo in _site, and push it to our server.
+  # Initialize a new git repo in dist, and push it to our server.
   cd dist
   git init
 
-  git remote add deploy "deploy@neutraltone.com:/var/www/codepen/html"
+  git remote add deploy "deploy@neutraltone.com:/var/www/codepen"
   git config user.name "Travis CI"
   git config user.email "tonyphipps+travisCI@gmail.com"
 
