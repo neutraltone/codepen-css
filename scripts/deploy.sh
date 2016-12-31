098e47316062ee88d2aae40f18fa9abea7e91d19
Tony Phipps <tonyphipps@gmail.com>
2016-12-31 00:10:20 +0000

    fix: deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
-    # Initialize a new git repo in _site, and push it to our server.
-    cd dist
-    git init
+  # Initialize a new git repo in _site, and push it to our server.
+  cd dist
+  git init
 
-    git remote add deploy "deploy@neutraltone.com:/var/www/codepen/html"
-    git config user.name "Travis CI"
-    git config user.email "tonyphipps+travisCI@gmail.com"
+  git remote add deploy "deploy@neutraltone.com:/var/www/codepen/html"
+  git config user.name "Travis CI"
+  git config user.email "tonyphipps+travisCI@gmail.com"
 
-    git add .
-    git commit -m "Deploy"
-    git push --force deploy master
+  git add .
+  git commit -m "Deploy"
+  git push --force deploy master
 else
-    echo "Not deploying, since this branch isn't master."
+  echo "Not deploying, since this branch isn't master."
 fi
